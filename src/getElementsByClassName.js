355ad0b27c3d7a89e61a92fcdb41e7a9ff25a872
techdeus <sullivan.marlon@gmail.com>
2018-09-17 12:20:02 -0700

    recursion review complated

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var docBody = document.body;
+  var results = [];
+  
+  var checkNode = function(node){
+    if ( node.classList && node.classList.contains(className) ) {
+      results.push(node);
+      console.log(results);
+    }
+    node.childNodes.forEach(function(child){
+      checkNode(child);
+    });
+  }
+  checkNode(docBody);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,25 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if ( Array.isArray(obj) ) {
+    var results = [];
+    obj.forEach(function(value) {
+      results.push(stringifyJSON(value));
+    });
+    return '['+results+']';
+  }
+  if (obj && typeof obj === 'object' ) {
+    var results = [];
+    for ( var key in obj ) {
+      if ( typeof obj[key] === undefined || typeof obj[key] === 'function' ) {
+        return '{}';
+      }
+      results.push( stringifyJSON(key) +':'+ stringifyJSON(obj[key]) ); 
+    }
+    return '{'+ results.join(',')+'}';
+  }
+  if ( typeof obj === 'string' ) {
+    return '"'+ obj+ '"';
+  }
+  return ''+obj;
 };
